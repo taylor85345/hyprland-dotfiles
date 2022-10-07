81190c8c769fac71f72923423b3289d6bb47339c
taylor85345 <taylor85345@hotmail.com>
2022-10-07 13:31:04 -0700

    remove deprecated volume scripts

--- dunst/scripts/vol_mute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DEFAULT_SINK=$(pactl info | grep "Default Sink" | cut -d " " -f3)
-pactl set-sink-mute "$DEFAULT_SINK" toggle
-dunstify -a "VOLUME" "MUTED" -h int:value:$volume -i audio-volume-muted-symbolic -r 2593 -u normal
-canberra-gtk-play -i audio-volume-change -d "changevolume"
