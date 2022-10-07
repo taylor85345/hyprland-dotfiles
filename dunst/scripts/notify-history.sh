718f835138fc03f75b2d76c4a8c23b166ea8644b
taylor85345 <taylor85345@hotmail.com>
2022-10-07 15:27:27 -0700

    Optimized dunst panel script

--- dunst/scripts/notify-history.sh
+++ dunst/scripts/notify-history.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-dunstctl history > /tmp/dunst-history.json
+socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r event; do 
+if [[ $event == "closelayer>>notifications" ]]; then
+  echo $event
+  dunstctl history > /tmp/dunst-history.json
+  exit
+fi
+done
